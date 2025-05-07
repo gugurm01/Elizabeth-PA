9b322be882add35673e81405199242d1e38bba58
Gustavo de Medeiros <gustavormedeiros2008@gmail.com>
2025-05-07 14:34:37 -0300

    pc

--- Lizas code venture/Assets/Gustavo/Scripts/Computer.cs
+++ Lizas code venture/Assets/Gustavo/Scripts/Computer.cs
@@ -1,11 +1,19 @@
 using Cinemachine;
+using System.Collections;
 using UnityEngine;
 
 public class Computer : MonoBehaviour, IInteractable
 {
-    private bool isInUse = false;
-
+    [Header("Camera & UI")]
     [SerializeField] private CinemachineVirtualCamera computerCamera;
+    [SerializeField] private CanvasGroup computerCanvas;
+
+    [Header("Timings")]
+    [SerializeField] private float fadeDuration = 0.5f;
+    [SerializeField] private float delayBeforeFade = 0.5f;
+
+    private bool isInUse = false;
+    private Coroutine fadeCoroutine;
 
     public void Interact()
     {
@@ -20,6 +28,7 @@ public class Computer : MonoBehaviour, IInteractable
         isInUse = true;
         Debug.Log("Entrou no computador.");
         ComputerManager.Instance.EnterComputer(this, computerCamera);
+        FadeCanvas(true);
     }
 
     private void ExitComputer()
@@ -27,11 +36,50 @@ public class Computer : MonoBehaviour, IInteractable
         isInUse = false;
         Debug.Log("Saiu do computador.");
         ComputerManager.Instance.ExitComputer();
+        FadeCanvas(false);
     }
 
     public void ExitComputerExternally()
     {
         isInUse = false;
         Debug.Log("Saiu do computador (externo).");
+        FadeCanvas(false);
+    }
+
+    private void FadeCanvas(bool fadeIn)
+    {
+        if (fadeCoroutine != null) StopCoroutine(fadeCoroutine);
+        fadeCoroutine = StartCoroutine(FadeCanvasRoutine(fadeIn));
+    }
+
+    private IEnumerator FadeCanvasRoutine(bool fadeIn)
+    {
+        if (fadeIn)
+        {
+            yield return new WaitForSeconds(delayBeforeFade);
+            computerCanvas.gameObject.SetActive(true);
+            computerCanvas.blocksRaycasts = true;
+            computerCanvas.interactable = true;
+        }
+
+        float start = fadeIn ? 0f : 1f;
+        float end = fadeIn ? 1f : 0f;
+        float time = 0f;
+
+        while (time < fadeDuration)
+        {
+            computerCanvas.alpha = Mathf.Lerp(start, end, time / fadeDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        computerCanvas.alpha = end;
+
+        if (!fadeIn)
+        {
+            computerCanvas.blocksRaycasts = false;
+            computerCanvas.interactable = false;
+            computerCanvas.gameObject.SetActive(false);
+        }
     }
 }
