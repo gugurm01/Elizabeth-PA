ef58f6a9ac328df502763f31450667df2c6665a0
Gustavo de Medeiros <gustavormedeiros2008@gmail.com>
2025-05-12 13:24:52 -0300

    Menu cabuloso

---
+++ Lizas code venture/Assets/Gustavo/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
